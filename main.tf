a9795ce616241657dd47120fb31b92bb12a925a1
ganeshboddu497 <126737181+ganeshboddu497@users.noreply.github.com>
2025-02-01 16:08:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+region = "us-west-1"
 }
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0f214d1b3d031dc53"
+ami = "ami-0810e059735ce3a9d"
 instance_type = "t2.medium"
-key_name = "rat"
-vpc_security_group_ids = ["sg-0895e3edee5da9d65"]
+key_name = "raj"
+vpc_security_group_ids = ["sg-0ef539053f942b101"]
 tags = {
 Name = var.instance_names[count.index]
 }
