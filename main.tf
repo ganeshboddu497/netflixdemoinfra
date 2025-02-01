293c83c3cf0d5f17e64831fa1975028cc6a08195
ganeshboddu497 <126737181+ganeshboddu497@users.noreply.github.com>
2025-02-01 16:39:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-west-1"
 resource "aws_instance" "one" {
 count = 5
 ami = "ami-0810e059735ce3a9d"
-instance_type = "t2.large"
+instance_type = "t2.micro"
 key_name = "raj"
 vpc_security_group_ids = ["sg-0ef539053f942b101"]
 tags = {
