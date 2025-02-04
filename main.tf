b31eb5e770189202d0bf59d5bcaacacb9bd15dec
ganeshboddu497 <126737181+ganeshboddu497@users.noreply.github.com>
2025-02-04 12:15:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0f214d1b3d031dc53 "
+ami = "ami-0f214d1b3d031dc53"
 instance_type = "t2.medium"
 key_name = "rat"
 vpc_security_group_ids = ["sg-0895e3edee5da9d65"]
